15229fc9320f6fc9bdc19bf883f4886af5df2131
TJHub <tjhuber2009@gmail.com>
2026-09-05 07:42:33 -0700

    Add v4 alpha-generation package

---
+++ moondev_autoresearch_reconstruction/v4/__init__.py
@@ -0,0 +1,7 @@
+"""AUTORESEARCH v4 alpha-generation package.
+
+V4 is an architecture fork from the frozen v3 research protocol.  It must not
+mutate v3 state or open hidden/final OOS data.
+"""
+
+PROTOCOL = "alpha_generation_v4"
